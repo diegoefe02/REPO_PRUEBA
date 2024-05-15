4b06110b0f31816fac080a64b8ef2a90edfe8b89
diegoefe02 <difermo-02@hotmail.com>
2024-05-15 19:04:48 +0200

    Crear repositorio

---
+++ contracts/interfaces/IIndicador.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Interfaz para el contrato Indicador
+interface IIndicador {
+    struct IOC {
+        string tipo;
+        string valor;
+        address reportadoPor;
+        uint256 fechaReporte;
+    }
+    
+    function agregarIOC(string memory tipo, string memory valor) external;
+    function getTiposPermitidos() external view returns (string[] memory);
+    function actualizarIOC(uint256 id, string memory tipo, string memory valor) external;
+    function eliminarIOC(uint256 id) external;
+    function obtenerIOC(uint256 id) external view returns (string memory tipo, string memory valor, address reportadoPor, uint256 fechaReporte);
+    function obtenerIOCPorValor(string memory valorBuscado) external view returns (string memory tipo, string memory valor, address reportadoPor, uint256 fechaReporte);
+    function obtenerTodosIOCs() external view returns (IOC[] memory);
+    function obtenerIDPorValor(string memory valorBuscado) external view returns (uint256);
+
+}
